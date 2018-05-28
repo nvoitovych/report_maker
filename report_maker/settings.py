74b2bfd87f3fdec0215c268999d5f65b83f6b601
Shelly <chumba.vumbaa@gmail.com>
2018-05-28 23:18:23 +0300

    Update settings.py

--- report_maker/settings.py
+++ report_maker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 's)r&fcx)7r@*ljuy)$lt+p+75bw5s8$p71l#(hh_c20$&&+wvl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['shellyshelly.pythonanywhere.com', ]
 
